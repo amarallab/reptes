a584488b479ee388bf3748733edc4f5e37ead221
Helio Tejedor <heltena@gmail.com>
2022-05-24 12:18:29 -0500

    Update Color+Extensions.swift

--- Sources/ReptesUI/Extensions/Color+Extensions.swift
+++ Sources/ReptesUI/Extensions/Color+Extensions.swift
@@ -8,26 +8,10 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-extension Color {
-//    public static let historicWave = Color("HistoricWave", bundle: Bundle.module)
-//
-//    public static let northwestern = Color("ShareNorthwestern", bundle: Bundle.module)
-//    public static let northwesternDark = Color("ShareNorthwesternDark", bundle: Bundle.module)
-//    public static let northwesternLight = Color("ShareNorthwesternLight", bundle: Bundle.module)
-//
-//    public static let waveViewBackground = Color(UIColor.systemGray5)
-//
+public extension Color {
     static let systemBackground = Color(UIColor.systemBackground)
     static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
 
     static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
     static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
-    
-//    public static func northwestern(for colorScheme: ColorScheme) -> Color {
-//        switch colorScheme {
-//        case .dark: return northwesternDark
-//        case .light: return northwesternLight
-//        @unknown default: return northwesternLight
-//        }
-//    }
 }
