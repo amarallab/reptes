5f9e59e295fd8331cd2ebd425e2659f671f24ab5
Helio Tejedor <heltena@gmail.com>
2022-05-25 10:15:24 -0500

    Added NavigationChallengeView

--- Sources/ReptesUI/ChallengeView.swift
+++ Sources/ReptesUI/ChallengeView.swift
@@ -29,46 +29,10 @@ fileprivate struct DynChallengeView: View {
     }
 }
 
-fileprivate struct SheetChallengeView: View {
-    @State var showing: Bool = false
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            Button {
-                showing = true
-            } label: {
-                Label("Show", systemImage: "eye")
-            }
-            Spacer()
-        }
-        .sheet(isPresented: $showing) {
-            NavigationView {
-                ChallengeView(challenge: .multipagePreview)
-                    .onButtonAction {
-                        if $0.action == .close {
-                            showing = false
-                        }
-                    }
-                    .navigationBarTitle("test", displayMode: .inline)
-                    .toolbar {
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button("Close") {
-                                showing = false
-                            }
-                        }
-                    }
-            }
-            .navigationViewStyle(.stack)
-        }
-    }
-}
-
 struct ChallengeView_Previews: PreviewProvider {
     static var previews: some View {
         DynChallengeView()
         ChallengeView(challenge: .missingBlockPreview)
         ChallengeView(challenge: .multipagePreview)
-        SheetChallengeView()
     }
 }
