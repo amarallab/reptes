801a77aeaf12a8f9dbcba2a316f5bae96a2a1bb8
Helio Tejedor <heltena@gmail.com>
2023-03-09 13:49:24 -0600

    Remove unnecessary code

--- Sources/ReptesUI/ChallengeCardView.swift
+++ Sources/ReptesUI/ChallengeCardView.swift
@@ -61,7 +61,6 @@ public struct ChallengeCardView: View {
             }
             .fixedSize(horizontal: false, vertical: true)
         }
-        .foregroundColor(card.foregroundColor?.color)
         .background(card.backgroundColor?.color ?? Color.accentColor)
         .cornerRadius(8.0)
         .contentShape(Rectangle())
